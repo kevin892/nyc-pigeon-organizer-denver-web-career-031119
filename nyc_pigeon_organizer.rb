276e46114a938becbef7a7bb6aa8fb3165761145
Kevin Ruggiero <kevinbruggiero@gmail.com>
2019-02-12 17:31:53 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,14 +5,14 @@ def nyc_pigeon_organizer(data)
   v.each do |x, y|
   array += y
   hash = Hash[array.map {|x| [x, nil]}]
-  hash["Theo"] = {color: ["purple", "grey"], gender: ["male"], lives: ["Subway"]}
-  hash["Peter Jr."] = {color: ["purple", "grey"], gender: ["male"], lives: ["Library"]}
-  hash["Lucky"] = {color: ["purple"], gender: ["male"], lives: ["Central Park"]}
-  hash["Ms. K"] = {color: ["grey", "white"], gender: ["female"], lives: ["Central Park"]}
-  hash["Queenie"] = {color: ["white", "brown"], gender: ["female"], lives: ["Subway"]}
-  hash["Andrew"] = {color: ["white"], gender: ["male"], lives: ["City Hall"]}
+              hash["Theo"] = {color: ["purple", "grey"], gender: ["male"], lives: ["Subway"]}
+            hash["Peter Jr."] = {color: ["purple", "grey"], gender: ["male"], lives: ["Library"]}
+          hash["Lucky"] = {color: ["purple"], gender: ["male"], lives: ["Central Park"]}
+        hash["Ms. K"] = {color: ["grey", "white"], gender: ["female"], lives: ["Central Park"]}
+      hash["Queenie"] = {color: ["white", "brown"], gender: ["female"], lives: ["Subway"]}
+    hash["Andrew"] = {color: ["white"], gender: ["male"], lives: ["City Hall"]}
   hash["Alex"] = {color: ["white", "brown"], gender: ["male"], lives: ["Central Park"]}
-end
-end
-return hash
+      end
+    end
+  return hash
 end
