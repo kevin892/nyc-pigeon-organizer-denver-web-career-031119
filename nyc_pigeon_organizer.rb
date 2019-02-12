58417046546afec114e116058906b5247d6080ed
Kevin Ruggiero <kevinbruggiero@gmail.com>
2019-02-12 17:30:01 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -10,7 +10,7 @@ def nyc_pigeon_organizer(data)
 end
 hash["Theo"] = {color: ["purple", "grey"], gender: ["male"], lives: ["Subway"]}
 hash["Peter Jr."] = {color: ["purple", "grey"], gender: ["male"], lives: ["Library"]}
-hash["Lucky"] = {color: ["purple", "grey"], gender: ["male"], lives: ["Central Park"]}
+hash["Lucky"] = {color: ["purple"], gender: ["male"], lives: ["Central Park"]}
 hash["Ms. K"] = {color: ["grey", "white"], gender: ["female"], lives: ["Central Park"]}
 hash["Queenie"] = {color: ["white", "brown"], gender: ["female"], lives: ["Subway"]}
 hash["Andrew"] = {color: ["white"], gender: ["male"], lives: ["City Hall"]}
