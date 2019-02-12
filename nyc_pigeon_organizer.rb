b6f1197f831834352c52fbdd75acfde4a476ec30
Kevin Ruggiero <kevinbruggiero@gmail.com>
2019-02-12 17:34:51 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,10 +1,10 @@
-def nyc_pigeon_organizer(data)
+def nyc_pigeon_organizer(data)#data is hash
   array =[]
   hash = {}
-  data.each do |k, v|
+  data.each do |k, v| 
   v.each do |x, y|
-  array += y
-  hash = Hash[array.map {|x| [x, nil]}]
+  array += y#add names to empyt array
+  hash = Hash[array.map {|x| [x, nil]}]#collect each name from array and create hash with names as key
               hash["Theo"] = {color: ["purple", "grey"], gender: ["male"], lives: ["Subway"]}
             hash["Peter Jr."] = {color: ["purple", "grey"], gender: ["male"], lives: ["Library"]}
           hash["Lucky"] = {color: ["purple"], gender: ["male"], lives: ["Central Park"]}
